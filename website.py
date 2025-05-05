c9aad8057c550a34f9a2a1708064669adbfa2000
AiwanSpartan <aiwanggeorge@gmail.com>
2025-05-05 20:04:10 +0100

    fix admin redirect

--- website.py
+++ website.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/<name>")
 def user(name):
-    return render_template("name.html", context = name)
+    return render_template("name.html", name = name)
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home"))
+    return redirect(url_for("user", name = "admin"))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
